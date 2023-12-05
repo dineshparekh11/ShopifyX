9d2e3dea1298bae57fb6d98c4199914fa095c3ab
Dinesh Parekh <dinesh21045@gmail.com>
2023-12-05 14:24:51 +0530

    first commit

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,19 @@
+import { ActionTypes } from '../constants/action-type'
+
+const initialState = {
+    products: [
+        {
+        id:1,
+        title: "Dinesh",
+        category: "Programmer",
+        },
+],
+}
+export const productReducer = (state = initialState, {type, payload}) => {
+   switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+        return state;
+    default:
+        return state;
+   }
+};
